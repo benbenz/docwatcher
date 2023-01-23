bd0188d0f216109fb00c100fce7f821d57711314
Benoit Simon <benoit@sxnths.com>
2023-01-23 20:54:19 +0100

    typo

--- crawler/helper.py
+++ crawler/helper.py
@@ -64,7 +64,7 @@ def call(session, url, use_proxy=False, retries=0):
             response.raise_for_status()
         except requests.exceptions.InvalidSchema as re:
             msg = str(re)
-            if url.startswith('tel':):
+            if url.startswith('tel:'):
                 pass
             else:
                 print(re)
