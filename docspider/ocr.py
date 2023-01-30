d70c05d60b69880b2a9c1c7a8faf859acf5bb0d3
Benoit Simon <benoit@sxnths.com>
2023-01-30 14:51:30 +0100

    debugging ocr

--- docspider/ocr.py
+++ docspider/ocr.py
@@ -17,7 +17,5 @@ ocr_reader  = easyocr.Reader(['fr'])
 result      = ocr_reader.readtext(args.img_path)
 bytes_out   = BytesIO()
 numpy.save(bytes_out,result)
-print(bytes_out.getvalue())
-print("RESULT="+bytes_out.getvalue().decode())
-
-
+#print(bytes_out.getvalue())
+print("RESULT=",bytes_out.getvalue())
