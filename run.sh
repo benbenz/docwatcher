80ec185375b1b484eb5383d3425978c0007f094b
Benoit Simon <benoit@sxnths.com>
2023-02-01 14:43:01 +0100

    back to simple run.sh (we can define work directory at the web hosting company)

--- run.sh
+++ run.sh
@@ -1,12 +1,5 @@
-if [[ $# < 1 ]];then
-    echo "Usage: run.sh DOCWATCHER_PATH"
-    exit
-fi
 deactivate
-root=$1
-shift # keep all the rest of the arguments under $@
-cd $root
-export PYTHONPATH=$root:$root/www:$PYTHONPATH
+export PYTHONPATH=$(pwd):$(pwd)/www:$PYTHONPATH
 source .venv/bin/activate
 source email_settings.sh
 python docspider/run.py $@
