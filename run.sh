ec3367b98a9c1ad0a77352eb00b60783332b4fbc
Benoit Simon <benoit@sxnths.com>
2023-02-01 16:11:45 +0100

    use update_index

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ export PYTHONPATH=$(pwd):$(pwd)/www:$PYTHONPATH
 source .venv/bin/activate
 source email_settings.sh
 python docspider/run.py $@
-python www/manage.py rebuild_index --noinput
+python www/manage.py update_index --noinput
 python docspider/search.py
