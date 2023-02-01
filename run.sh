fbec283e68240dabde09b6cd0378d5b1117659e4
Benoit Simon <benoit@sxnths.com>
2023-02-01 14:40:38 +0100

    run.sh more modular

--- run.sh
+++ run.sh
@@ -1,7 +1,14 @@
+if [[ $# < 1 ]];then
+    echo "Usage: run.sh DOCWATCHER_PATH"
+    exit
+fi
 deactivate
-export PYTHONPATH=$(pwd):$(pwd)/www:$PYTHONPATH
+root=$1
+shift # keep all the rest of the arguments under $@
+cd $root
+export PYTHONPATH=$root:$root/www:$PYTHONPATH
 source .venv/bin/activate
 source email_settings.sh
-python docspider/run.py
+python docspider/run.py $@
 python www/manage.py rebuild_index --noinput
 python docspider/search.py
