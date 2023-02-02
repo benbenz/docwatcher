7067c903c1bd06f3a0b2181bc7d3a76f1182016a
Benoit Simon <benoit@sxnths.com>
2023-02-02 11:57:59 +0100

    removing download directory in activate.sh

--- activate.sh
+++ activate.sh
@@ -4,6 +4,7 @@ source .venv/bin/activate
 source email_settings.sh
 git pull origin master
 rm www/db.sqlite3
+rm -rf download
 rm -rf www/docs/migrations
 python www/manage.py makemigrations docs
 python www/manage.py migrate
