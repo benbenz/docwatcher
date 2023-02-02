0b1d4f9adca574edcfa73c8d7eb28012861d68c0
Benoit Simon <benoit@sxnths.com>
2023-02-02 15:52:48 +0100

    Deleting the whoosh_index on reset

--- activate.sh
+++ activate.sh
@@ -6,6 +6,7 @@ git pull origin master
 rm www/db.sqlite3
 rm -rf download
 rm -rf www/docs/migrations
+rm -rf www/docwatcher/whoosh_index
 python www/manage.py makemigrations docs
 python www/manage.py migrate
 python docspider/init.py
