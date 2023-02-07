2da6345759a2fbe522d6783bb2c25f0960cee0ec
Benoit Simon <benoit@sxnths.com>
2023-02-07 11:14:50 +0100

    change of source > .

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 deactivate
 export PYTHONPATH=$(pwd):$(pwd)/www:$PYTHONPATH
-source .venv/bin/activate
-source email_settings.sh
+. .venv/bin/activate
+. email_settings.sh
 python docspider/run.py $@
 if [[ -z "${SKIP_SEARCH}" ]]; then
-    python www/manage.py update_index --noinput
+    # python www/manage.py rebuild_index --noinput
+    python www/manage.py update_index
     python docspider/search.py
 fi
