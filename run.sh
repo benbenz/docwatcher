f2cb58f0a8191def7ce240d1978eb2d8e8d4c0d6
Benoit Simon <benoit@sxnths.com>
2023-02-07 10:12:31 +0100

    index update included under SKIP_SEARCH clause

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ export PYTHONPATH=$(pwd):$(pwd)/www:$PYTHONPATH
 source .venv/bin/activate
 source email_settings.sh
 python docspider/run.py $@
-python www/manage.py update_index --noinput
 if [[ -z "${SKIP_SEARCH}" ]]; then
+    python www/manage.py update_index --noinput
     python docspider/search.py
 fi
