e3c6576210bcadf736704e4f745d7e83d53531a9
Benoit Simon <benoit@sxnths.com>
2023-02-08 16:49:33 +0100

    adding original URL to email

--- docspider/searchers.py
+++ docspider/searchers.py
@@ -121,8 +121,8 @@ class DocumentSearcher:
                 html_content += '<h4>Ajoutés:</h4><ul>'
                 for doc in docs_add:
                     title = str(doc.title or doc.remote_name or 'Sans titre')
-                    text_content += '\n' + str(doc.domain)+': ' + title + ': ' + urlroot + doc.get_absolute_url()
-                    html_content += '<li>'+str(doc.domain)+': <a href="'+urlroot+doc.get_absolute_url()+'">'+title+'</a></li>'
+                    text_content += '\n' + str(doc.domain)+': ' + title + ': ' + urlroot + doc.get_absolute_url() + ' original url: ' + doc.url
+                    html_content += '<li>'+str(doc.domain)+': <a href="'+urlroot+doc.get_absolute_url()+'">'+title+'</a> (<a href="'+doc.url+'">original URL</a>)</li>'
                     if highlights_s.get(doc.id):
                         highlight_text = highlights_s.get(doc.id)
                         highlight_text = highlight_text.replace('<em>','<span style="font-weight:800">')
