b682a024560cc4d1364ee5d0a783645bfb2a7715
Benoit Simon <benoit@sxnths.com>
2023-02-08 14:37:58 +0100

    testing for OCR activation in run_ocr

--- docspider/run_ocr.py
+++ docspider/run_ocr.py
@@ -8,6 +8,10 @@ def perform_ocr():
 
     handler = AllInOneHandler(directory=output_dir, subdirectory=None)
 
+    if not handler.using_ocr:
+        print("Not using OCR >> exiting")
+        return
+
     # we only perform OCR on PDF for now
     for doc in handler.get_documents(doc_types=[Document.DocumentType.PDF],for_ocr=True):
         # re-perform OCR 
