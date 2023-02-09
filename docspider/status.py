c7574f8a36e45ea288917c9d11fca459c032f446
Benoit Simon <benoit@sxnths.com>
2023-02-09 10:49:59 +0100

    improved status to check for site map

--- docspider/status.py
+++ docspider/status.py
@@ -7,11 +7,12 @@ from urllib.parse import urlparse
 from crawler.core import CrawlerMode
 
 
-def get_status():
+def get_status(links_to_check=None):
     filesOfDirectory = os.listdir('.')
-    pattern = "state.*"
+    pattern_state = "state.*"
+    pattern_sitemap = "sitemap.*"
     for file in filesOfDirectory:
-        if fnmatch.fnmatch(file, pattern):
+        if fnmatch.fnmatch(file, pattern_state):
             with open(file,'rb') as f:
                 crawler = pickle.load(f)
                 crawler_mode = CrawlerMode( crawler.crawler_mode & (CrawlerMode.CRAWL_ALL - CrawlerMode.CRAWL_RECOVER) )
@@ -32,5 +33,24 @@ def get_status():
                 logger.info("sleep_time = {0}".format(crawler.sleep_time))
                 logger.info("num urls_to_recover = {0}".format(len(crawler.urls_to_recover)))
 
+    for file in filesOfDirectory:
+        if fnmatch.fnmatch(file, pattern_sitemap) and links_to_check:
+            with open(file,'rb') as f:
+                sitemap = pickle.load(f)
+                for url , links in sitemap.items():
+                    for link in links:
+                        if link in links_to_check:
+                            print("{0} is included in {1} links ({2})".format(link,url,file))
+
 if __name__ == '__main__':
-    get_status()
+
+    parser = argparse.ArgumentParser(prog = 'DocWatcher Status',description = 'Retrieve status',epilog = '=)')
+    parser.add_argument('-l','--links',help="check if urls are linked in sitemap")
+    args = parser.parse_args()    
+
+    if args.links is not None:
+        links = args.links.split(',')
+    else:
+        links = None
+
+    get_status(links)
