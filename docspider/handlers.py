bad3eac41a6026616f52e4437803b8556327ec34
Benoit Simon <benoit@sxnths.com>
2023-02-09 11:43:00 +0100

    more debug prints

--- docspider/handlers.py
+++ docspider/handlers.py
@@ -471,6 +471,7 @@ class AllInOneHandler(LocalStorageHandler):
                     the_doc.last_modified = last_modified
                     the_doc.is_handled = True
                     the_doc.save() 
+                logger.debug("updated existing document {0} {1}".format(path,the_doc.url))
                 return path , file_status , the_doc.id
             except:
                 logger.error("INTERNAL Error: an existing file is not registered in the database!")
