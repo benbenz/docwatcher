19dd86440758c2e6fc97210910ffcaf123d0eb23
Benoit Simon <benoit@sxnths.com>
2023-02-10 09:39:44 +0100

    tuned up reset

--- reset.sh
+++ reset.sh
@@ -1,9 +1,10 @@
-deactivate
+# deactivate
+# export PYTHONPATH=$(pwd):$(pwd)/www:$PYTHONPATH
+# source .venv/bin/activate
+# source email_settings.sh
+source ./env.sh
 ps -ef | grep 'docspider/run.py' | grep -v grep | awk '{print $2}' | xargs -r kill -9
 ps -ef | grep 'docspider/ocr.py' | grep -v grep | awk '{print $2}' | xargs -r kill -9
-export PYTHONPATH=$(pwd):$(pwd)/www:$PYTHONPATH
-source .venv/bin/activate
-source email_settings.sh
 git pull origin master
 rm www/db.sqlite3
 rm -rf download
@@ -15,4 +16,3 @@ rm *.log
 python www/manage.py makemigrations docs
 python www/manage.py migrate
 python docspider/init.py
-# python docspider/run.py
