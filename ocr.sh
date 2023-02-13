623b9c575dac6ba7360a88596274ca41f8b6903e
Benoit Simon <benoit@sxnths.com>
2023-02-13 17:51:32 +0100

    removed search from OCR background thread

--- ocr.sh
+++ ocr.sh
@@ -5,9 +5,9 @@
 # source email_settings.sh
 source ./env.sh
 python docspider/run_ocr.py $@
-if [[ -z "${SKIP_SEARCH}" ]]; then
-    python www/manage.py update_index
-    python docspider/search.py
-else
-    echo "ocr.sh: not performing search"
-fi
+# if [[ -z "${SKIP_SEARCH}" ]]; then
+#     python www/manage.py update_index
+#     python docspider/search.py
+# else
+#     echo "ocr.sh: not performing search"
+# fi
