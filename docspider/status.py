69ff675ba06467a1f613835b3439891add384b64
Benoit Simon <benoit@sxnths.com>
2023-02-14 11:31:16 +0100

    catch error when reading pickle file

--- docspider/status.py
+++ docspider/status.py
@@ -24,32 +24,35 @@ def get_status(links_to_check=None):
     ljustsize = 20
     for file in filesOfDirectory:
         if fnmatch.fnmatch(file, pattern_state):
-            with open(file,'rb') as f:
-                crawler = pickle.load(f)
-                crawler_mode = CrawlerMode( crawler.crawler_mode & (CrawlerMode.CRAWL_ALL - CrawlerMode.CRAWL_RECOVER) )
-                if crawler.crawler_mode & CrawlerMode.CRAWL_RECOVER:
-                    crawler_mode_rec = CrawlerMode.CRAWL_RECOVER
-                else:
-                    crawler_mode_rec = None
-                domain = crawler.get_domain()
-                if not domain:
-                    domain = file
-                print(bcolors.HEADER+"\n{0}:".format(domain)+bcolors.CEND)
-                if crawler.has_finished:
-                    print("has_finished".ljust(ljustsize),":",bcolors.BOLD+bcolors.OKCYAN+"{0}".format(crawler.has_finished)+bcolors.CEND)
-                else:
-                    print("has_finished".ljust(ljustsize),":","{0}".format(crawler.has_finished))
-                if crawler_mode_rec:
-                    print("mode".ljust(ljustsize),":","{0} | {1}".format(crawler_mode.name,CrawlerMode.CRAWL_RECOVER.name))
-                else:
-                    print("mode".ljust(ljustsize),":","{0}".format(crawler_mode.name))
-                print("safe".ljust(ljustsize),":","{0}".format(crawler.safe))
-                print("sleep_time".ljust(ljustsize),":","{0}".format(crawler.sleep_time))
-                print("num urls_to_recover".ljust(ljustsize),":","{0}".format(len(crawler.urls_to_recover)))
+            try:
+                with open(file,'rb') as f:
+                    crawler = pickle.load(f)
+                    crawler_mode = CrawlerMode( crawler.crawler_mode & (CrawlerMode.CRAWL_ALL - CrawlerMode.CRAWL_RECOVER) )
+                    if crawler.crawler_mode & CrawlerMode.CRAWL_RECOVER:
+                        crawler_mode_rec = CrawlerMode.CRAWL_RECOVER
+                    else:
+                        crawler_mode_rec = None
+                    domain = crawler.get_domain()
+                    if not domain:
+                        domain = file
+                    print(bcolors.HEADER+"\n{0}:".format(domain)+bcolors.CEND)
+                    if crawler.has_finished:
+                        print("has_finished".ljust(ljustsize),":",bcolors.BOLD+bcolors.OKCYAN+"{0}".format(crawler.has_finished)+bcolors.CEND)
+                    else:
+                        print("has_finished".ljust(ljustsize),":","{0}".format(crawler.has_finished))
+                    if crawler_mode_rec:
+                        print("mode".ljust(ljustsize),":","{0} | {1}".format(crawler_mode.name,CrawlerMode.CRAWL_RECOVER.name))
+                    else:
+                        print("mode".ljust(ljustsize),":","{0}".format(crawler_mode.name))
+                    print("safe".ljust(ljustsize),":","{0}".format(crawler.safe))
+                    print("sleep_time".ljust(ljustsize),":","{0}".format(crawler.sleep_time))
+                    print("num urls_to_recover".ljust(ljustsize),":","{0}".format(len(crawler.urls_to_recover)))
 
-                crawl_tree = getattr(crawler,'crawl_tree',None)
-                current_url = get_first_non_ready_crawl_node(crawl_tree)
-                print("current URL".ljust(ljustsize),":","{0}".format(current_url))
+                    crawl_tree = getattr(crawler,'crawl_tree',None)
+                    current_url = get_first_non_ready_crawl_node(crawl_tree)
+                    print("current URL".ljust(ljustsize),":","{0}".format(current_url))
+            except:
+                print(bcolors.FAIL+"error while loading {0}".format(file)+bcolors.CEND)
 
     for file in filesOfDirectory:
         if fnmatch.fnmatch(file, pattern_sitemap) and links_to_check:
